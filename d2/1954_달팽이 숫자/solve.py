4bc71ec57988d7dc94c6171a3ba32b25250fcbd7
김혜련 <hyeryeons23@gmail.com>
2019-08-29 14:03:39 +0900

    Modify SWEA No.1974

--- d2/1954_달팽이 숫자/solve.py
+++ d2/1954_달팽이 숫자/solve.py
@@ -7,33 +7,35 @@ for T in range(1, int(input())+1):
     num_list = list(range(1, N ** 2 + 1))  # n x n 숫자를 list에 넣음
 
     idx = 0
-    c = 0
     # 짝수 k = n / 2
     # 홀수 k = (n - 1) / 2
  
     if N % 2 == 0:  # N x N 이 짝수
-        k = int(N / 2)  # 4면 2번만 넣으면 됨
+        H = int(N / 2)  # 4면 2번만 넣으면 됨
     else:
-        k = int((N - 1) / 2)  # 3이면 N-1 가운데 값
+        H = int((N - 1) / 2)  # 3이면 N-1 가운데 값
  
-    for c in range(k):
-        for j in range(c, N - c):
-            arr[c][j] = num_list[idx]
+    for h in range(H):  # 4일 때 2번 돌리면 됨
+        # 열증가(행 그대로)
+        for j in range(h, N - h):
+            arr[h][j] = num_list[idx]
             idx += 1
-        for i in range(c + 1, N - c):
-            arr[i][N - c-1] = num_list[idx]
+        # 행증가(열 그대로)
+        for i in range(h + 1, N - h):
+            arr[i][N - h-1] = num_list[idx]
             idx += 1
-        for j in range(N - 2 - c, c - 1, -1):
-            arr[N - c-1][j] = num_list[idx]
+        # 열감소(행 그대로)
+        for j in range(N - 2 - h, h - 1, -1):
+            arr[N - h-1][j] = num_list[idx]
             idx += 1
-        for i in range(N - 2 - c, c, -1):
-            arr[i][c] = num_list[idx]
+        # 행감소(열 그대로)
+        for i in range(N - 2 - h, h, -1):
+            arr[i][h] = num_list[idx]
             idx += 1
-        c += 1
  
     if N % 2 == 1:  # N x N 이 홀수
-        arr[k][k] = num_list[idx]  # 가운데 값 = num_list idx의 끝값
+        arr[H][H] = num_list[idx]  # 가운데 값 = num_list idx의 끝값
 
-    print(f'#{T}')
+    print('#{}'.format(T))
     for i in range(N):
         print(' '.join(map(str,arr[i])))
--- d2/1974_스도쿠검증/solve.py
+++ d2/1974_스도쿠검증/solve.py
@@ -1,56 +1,43 @@
-'''
-10
-4 5 7 1 6 3 8 2 9
-6 3 9 8 2 7 5 4 1
-7 9 3 4 8 5 1 6 2
-1 8 2 5 4 9 6 3 7
-8 6 1 7 9 2 3 5 4
-5 2 4 6 3 1 7 9 8
-3 7 6 9 1 4 2 8 5
-2 4 5 3 7 8 9 1 6
-9 1 8 2 5 6 4 7 3
-'''
 import sys
 sys.stdin = open('input.txt', 'r')
 
-for T in range(1, int(input())+1):
+
+TC = int(input())
+
+for t in range(1, TC+1):
+    # arr 받기
+    row_arr = [list(map(int, input().split())) for i in range(9)]
     total = 45
     result = True
-    N = 9
-    
-    # 행조사
-    row_lst = list(list(map(int, input().split())) for i in range(N))
-    for r in row_lst:
-        if sum(r) != 45:
-            result = False 
-            break
-    
-    if result:  # 만약 result 가 1이라면,
-        #열조사
-        col_lst = list(map(list, zip(*row_lst)))
-        for c in col_lst:
-            if sum(c) != 45:
-                result = False 
-                break
-    
-    # 가로줄 3행씩 조사
-    if result:
-        k = 0
-        l = 0
-        arr = []
-        for i in range(k, k + 3):
-            for j in range(l, l + 3):
-                arr.append(row_lst[i][j])
-        # print(arr)  # [4, 5, 7, 6, 3, 9, 7, 9, 3]
-        if sum(arr) != total:
+
+    # 가로줄 조사
+    for row in row_arr:
+        if sum(row) != total:
             result = False
-        else:
-            arr = []  # 다시 빈 리스트로 초기화
-            k = 0  # range 초기화
-            l += 3  # 열 범위 + 해주기
-        if l > 9:  # 열 길이가 9 이상이 되면, result 는 1이고, 다음 단계로 넘어가기!
-            result = True
-            k += 3  # 행 범위 + 해주기
+            break
 
+    # 세로줄 조사
+    col_arr = list(map(list, zip(*row_arr)))
+    for col in col_arr:
+        if sum(col) != total:
+            result = False
+            break
 
-    print('#{} {}'.format(T, int(result)))
+    # 33 조사
+
+    # 시작점 찾기
+    for i in range(0, 9, 3):  # 0, 3, 6
+        if result == True:
+            for j in range(0, 9, 3):
+                sum1 = 0  # 초기화
+                # 시작점 기준 돌릴 범위 설정하기
+                for x in range(3):
+                    for y in range(3):
+                        sum1 += row_arr[i + x][j + y]
+                if sum1 != total:
+                    result = False
+                    break
+                else:
+                    result = True
+
+    print('#{} {}'.format(t, int(result)))
